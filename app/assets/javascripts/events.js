2a5ed4f8a70f962d6f4f2771f4b865429863d783
benja83 <tarennebenjamin@gmail.com>
2014-08-21 18:02:18 +0200

    small changes, passed flat script to event.js to flat.js

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -40,19 +40,3 @@ $(document).ready(function () {
 var closeForm = function(){
       $('#side-event').addClass('sr-only').removeClass('sidebar-left-active');
     };
-$('.sidebar-bottom-right').hover(
-  function(){
-  $('.sidebar-bottom-right').addClass('sidebar-bottom-right-active').removeClass('sidebar-bottom-right');
-  },
-  function(){
-  $('.sidebar-bottom-right-active').addClass('sidebar-bottom-right').removeClass('sidebar-bottom-right-active');
-  }
-);
-$('.sidebar-bottom-left').hover(
-  function(){
-    $('.sidebar-bottom-left').addClass('sidebar-bottom-left-active').removeClass('sidebar-bottom-left');
-  },
-  function(){
-    $('.sidebar-bottom-left-active').addClass('sidebar-bottom-left').removeClass('sidebar-bottom-left-active');
-  }
-);
---
+++ app/assets/javascripts/flats.js
@@ -0,0 +1,17 @@
+$('.sidebar-bottom-right').hover(
+  function(){
+  $('.sidebar-bottom-right').addClass('sidebar-bottom-right-active').removeClass('sidebar-bottom-right');
+  },
+  function(){
+  $('.sidebar-bottom-right-active').addClass('sidebar-bottom-right').removeClass('sidebar-bottom-right-active');
+  }
+);
+$('.sidebar-bottom-left').hover(
+  function(event){
+    $('.sidebar-bottom-left').addClass('sidebar-bottom-left-active').removeClass('sidebar-bottom-left');
+    event.stopPropagation();
+  },
+  function(){
+    $('.sidebar-bottom-left-active').addClass('sidebar-bottom-left').removeClass('sidebar-bottom-left-active');
+  }
+);
