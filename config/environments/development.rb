b58e58d2f2024dcd9e68a7e91b1cf72ecc234d1f
benja83 <tarennebenjamin@gmail.com>
2014-08-22 19:36:27 +0200

    added config for heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
 end
