5e23181fec21456522250f0c0decfcde2495e7af
benja83 <tarennebenjamin@gmail.com>
2014-08-24 13:24:12 +0200

    finished balance_date method to get data for create balances

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -12,8 +12,15 @@ scope :month, -> (date) { where("created_at >= ? AND created_at <= ?",
   date.at_beginning_of_month, date.end_of_month) }
 
 
-  def self.balance_data
-    {:total => 10}
+  def self.balance_data(date = Date.today)
+    total = Bill.month(date).sum(:price)
+    data ={:total => total}
+    users_id = Bill.month(date).pluck('DISTINCT user_id')
+    users_id.each do |id|
+      data[id] = Bill.month(date).where(user_id: id).sum(:price)
+    end
+    return data
+
   end
 
 end
