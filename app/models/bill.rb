7526e21085f6cbd9e767fa20e6242f6f529d4e44
benja83 <tarennebenjamin@gmail.com>
2014-08-24 12:42:30 +0200

    added scope to filter bills by month pass in param

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -8,6 +8,10 @@ class Bill < ActiveRecord::Base
 scope :current_month, -> { where("created_at >= ? AND created_at <= ?",
   Date.today.at_beginning_of_month, Date.today.end_of_month) }
 
+scope :month, -> (date) { where("created_at >= ? AND created_at <= ?",
+  date.at_beginning_of_month, date.end_of_month) }
+
+
   def self.balance_data
     {:total => 10}
   end
