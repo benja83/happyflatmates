d3dcc0160ebb1ea78427ec6a69b3599e3945491b
benja83 <tarennebenjamin@gmail.com>
2014-08-25 10:52:22 +0200

    created create_balances_users_per_flat to get an hash with balances of each user per flat

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -1,8 +1,18 @@
 class Balance < ActiveRecord::Base
 
 
-  def self.create_balances(date = Date.today)
-    data = Bill.balance_data
+  def self.create_balances_users_per_flat(date = Date.today, flat_id)
+    array_data = Bill.balance_data
+    balances_users_per_flat = {}
+    array_data.each_index do |i|
+      if array_data[i][:flat_id] == flat_id
+        array_data[i][:users_total].each do |key,value|
+          user_balance = value -  array_data[i][:total_per_user]
+          balances_users_per_flat[key] = user_balance
+        end
+      end
+    end
+    return balances_users_per_flat
 
   end
 
--- spec/models/balance_spec.rb
+++ spec/models/balance_spec.rb
@@ -2,4 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Balance, :type => :model do
 
+  context "create_balances_users_per_flat" do
+
+    before(:each) do
+      Flat.destroy_all
+      User.destroy_all
+
+      @flat1 = Flat.create name: "marina", address: "carrer de la marina 200 Barcelona"
+      @flat2 = Flat.create name: "mob", address: "carrer de la marina 200 Barcelona"
+      @user1 = User.create name:"Jane", email: "jane.buzzlightyear@gmail.com", flat_id: @flat1.id
+      @user2 = User.create name:"Jane", email: "greg@gmail.com", flat_id: @flat1.id
+      @bill1 = Bill.create item: 'mercadona',price: 10, flat_id: @flat1.id, user_id: @user1.id
+      @bill2 = Bill.create item: 'mercadona',price: 20, flat_id: @flat1.id, user_id: @user2.id
+    end
+
+    it "It generate an hash with the differences between total_per_user with total of each user" do
+
+      hash = Balance.create_balances_users_per_flat(nil,@flat1.id)
+      expect(hash[@user1.id]).to eq(-5)
+      expect(hash[@user2.id]).to eq(5)
+    end
+  end
+
 end
